c95a65e77ff9b010132fc4ddb07e41ef5abbf04e
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-13 15:20:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+    let message;
+    if (feet <= 400) {
+      message = "This one is on me!";
+    } else if (feet > 2000 && feet <= 2500) {
+      message = "I will gladly take your thirty bucks.";
+    } else if (feet >2500) {
+      message = "No can do.";
+    }
+    return message;
+  }
+  
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good.": "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
